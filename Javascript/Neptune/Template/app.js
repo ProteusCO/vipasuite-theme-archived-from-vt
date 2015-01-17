a0e96bae3b14d25d9dffa60a527ff10feafc29d8
Scott Benes <sbenes+github@vipasolutions.com>
2015-01-17 17:24:26 -0600

    added select2 4.x code, have to disable temporarily til bug is fixed

--- Javascript/Neptune/Template/app.js
+++ Javascript/Neptune/Template/app.js
@@ -1,7 +1,17 @@
 (function($, w, d) {
-	var $appControlsCon, $siteLocaleComponent, $nav;
+	var $appControlsCon, $siteLocaleComponent, $navCon;
+	var $appContentCon;
 
 	var CSS_CLASS_HAS_SITE_LOCALE = 'has-site-locale';
+	var CSS_CLASS_SELECT_INIT = 'select2-init';
+
+	var DEFAULT_SELECT_OPTIONS = {
+		//width: function() {
+		//	return parseInt(this.element.width(), 10) + 15;
+		//},
+		theme: 'vipa',
+		minimumResultsForSearch: 10
+	};
 
 	function Menu(target, opts) {
 		var $root;
@@ -65,16 +75,50 @@
 		init();
 	}
 
-	function init() {
+	function destroySelectUpdates(context) {
+		var $con = $(context || document);
+
+		if (!$con.hasClass(CSS_CLASS_SELECT_INIT)) {
+			$con = $con.find('select').filter('.' + CSS_CLASS_SELECT_INIT);
+		}
+
+		if ($con.length) {
+			$con.removeClass(CSS_CLASS_SELECT_INIT).select2('destroy');
+		}
+	}
+
+	function initSelectUpdates(context) {
+		var $con = $(context || document);
+
+		if (!$con.is('select')) {
+			$con = $con.find('select');
+		}
+
+		if ($con.length) {
+			$con
+					.select2(DEFAULT_SELECT_OPTIONS)
+					.addClass(CSS_CLASS_SELECT_INIT)
+					.filter('[data-features="watch"]')
+					.on('change', miwt.observerFormSubmit);
+		}
+	}
+
+	function initControls() {
+		//find elements
 		$appControlsCon = $('.e-app-controls');
 		$siteLocaleComponent = $appControlsCon.find('.site-locale');
-		$nav = $('.l-nav');
+		$navCon = $('.l-nav');
 
+		//set up menu
+		new Menu('.primary-nav');
+
+		//add state information to controls
 		if ($siteLocaleComponent.length) {
 			$appControlsCon.addClass(CSS_CLASS_HAS_SITE_LOCALE);
 		}
 
-		$nav.mCustomScrollbar({
+		//set up scrollbar on menu
+		$navCon.mCustomScrollbar({
 			scrollInertia: 0,
 			mouseWheel: {
 				scrollAmount: 100
@@ -82,9 +126,33 @@
 		});
 	}
 
-	$(d).on('vs:init-controls', function() {
-		init();
-		new Menu('.primary-nav');
+	function initContent() {
+		//find elements
+		$appContentCon = $('.e-app-content');
+
+		//set up miwt interactions
+		$appContentCon.find('form.miwt-form').each(function() {
+			this.submit_options = {
+				preProcessNode: function(data) {
+					destroySelectUpdates(document.getElementById(data.refid));
+					return data.content;
+				},
+				postProcessNode: function(data) {
+					$.each(data, function(idx, d) {
+						initSelectUpdates(d.node);
+					});
+				}
+			};
+
+			initSelectUpdates(this);
+		});
+	}
+
+	$(d).on('vs:init-controls', initControls).on('vs:init-content', initContent);
+
+	$(d).ready(function() {
+		//need select 2 to be fixed for programmatic init
+		//$(this).trigger('vs:init-content');
 	});
 
 })(jQuery, window, document);
