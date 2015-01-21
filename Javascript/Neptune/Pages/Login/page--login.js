9084e2b5ed60021818efe1f2f046d5f5da6b1187
Scott Benes <sbenes+github@vipasolutions.com>
2015-01-20 21:27:25 -0600

    updates to the login pages

---
+++ Javascript/Neptune/Pages/Login/page--login.js
@@ -0,0 +1,24 @@
+jQuery(function($) {
+	var placeholders = [
+		{
+			selector: '.password-entry .field.username input',
+			text: 'Username'
+		},
+		{
+			selector: '.password-entry .field.password input',
+			text: 'Password'
+		},
+		{
+			selector: '.password-reset .field.password input',
+			text: 'New Password'
+		},
+		{
+			selector: '.password-reset .field.verify-password input',
+			text: 'Verify New Password'
+		}
+	];
+
+	$.each(placeholders, function(idx, placeholder) {
+		$(placeholder.selector).attr('placeholder', placeholder.text);
+	});
+});
---
+++ Javascript/Neptune/Pages/Login/page--reset-password.js
@@ -0,0 +1,12 @@
+jQuery(function($) {
+	var placeholders = [
+		{
+			selector: '.retrieve-password .email input',
+			text: 'Email'
+		}
+	];
+
+	$.each(placeholders, function(idx, placeholder) {
+		$(placeholder.selector).attr('placeholder', placeholder.text);
+	});
+});
