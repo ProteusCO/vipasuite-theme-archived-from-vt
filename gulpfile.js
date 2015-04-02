61473bdbdbed851f562c9f42f44b09b8710d3103
Scott Benes <sbenes+github@vipasolutions.com>
2015-04-01 20:58:38 -0500

    added gulp and a gulp file to compile sass files

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', ['styles']);
+
+gulp.task('styles', function () {
+  return gulp.src('./Stylesheets/src/**')
+      .pipe(sass({
+			  outputStyle: 'nested'
+		  }))
+      .pipe(autoprefixer({
+        browsers: ['> 1%', 'last 2 versions', 'ie >= 9'],
+        cascade: false
+      }))
+      .pipe(gulp.dest('./Stylesheets/build'));
+});
