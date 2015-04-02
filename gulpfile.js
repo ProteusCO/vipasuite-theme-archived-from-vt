d7fcc6850b6aa2b498fa339a1263d4dc6de1f306
Scott Benes <sbenes+github@vipasolutions.com>
2015-04-02 00:19:07 -0500

    update to use gulp webfont builder
    - Files are renmaed via the gulp webfont plugin to prevent shuffling of the icons once used.
    - Use a template now to generate the SCSS file for the font glyph entities
    - We now get SVG font generation as well

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
+var _ = require('underscore');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var iconfont = require('gulp-iconfont');
+var consolidate = require('gulp-consolidate');
+var rename = require('gulp-rename');
 
 gulp.task('default', ['styles']);
 
@@ -14,4 +18,30 @@ gulp.task('styles', function () {
         cascade: false
       }))
       .pipe(gulp.dest('./Stylesheets/build'));
+});
+
+gulp.task('iconfont', function(){
+	return gulp.src('./FontGlyphs/src/icons/*.svg')
+			.pipe(iconfont({
+				fontName: 'neptune-glyph-font', // required
+				appendCodepoints: true, // recommended option
+				startCodepoint: 0xF101,
+				fontHeight: 150
+			}))
+			.on('codepoints', function(codepoints, options) {
+				codepoints = _.map(codepoints, function(codepoint) {
+					return {
+						name: codepoint.name,
+						codepoint: codepoint.codepoint.toString(16).toLowerCase()
+					};
+				});
+
+				gulp.src('./FontGlyphs/src/templates/_iconfont.scsstpl')
+						.pipe(consolidate('lodash', {
+							glyphs: codepoints
+						}))
+						.pipe(rename('_font-glyph-entities.scss'))
+						.pipe(gulp.dest('./Stylesheets/src/Neptune/Config/'));
+			})
+			.pipe(gulp.dest('./Design/Neptune/Fonts/GlyphLib/'));
 });
